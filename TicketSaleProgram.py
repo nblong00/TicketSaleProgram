7a283317b7aa9e7297162a06b0d8687ac487f64d
Blaise <blazzze0517@gmail.com>
2025-05-03 22:26:22 -0400

    Pushing main TicketSale simulator. Allows you to 'purchase' fake tickets

---
+++ TicketSaleProgram.py
@@ -0,0 +1,27 @@
+TICKET_PRICE = 10
+SERVICE_CHARGE = 2
+
+tickets_remaining = 100 
+
+def calculate_price(number_of_tickets):
+    return (number_of_tickets * TICKET_PRICE) + 2
+
+while tickets_remaining != 0:
+    print("Number of tickets remaining: {}".format(tickets_remaining))
+    user_name = input("Enter your name: ")
+    try:
+        tickets_needed = int(input("Hello {}, how many tickets are you looking to purchase? ".format(user_name)))
+        if tickets_needed > tickets_remaining:
+            raise ValueError("Requested ticket amount is above what is available. Try again.")
+    except ValueError as e: 
+        print("Invalid entry. {}".format(e))
+    else:
+        total_price = calculate_price(tickets_needed) 
+        print("Your total is ${}".format(total_price))
+        proceed = input("Would you like to proceed with purchase, {}?\n(Enter yes/no) ".format(user_name))
+        if proceed.lower() == 'yes':
+            print("Your tickets have been purchased.")
+            tickets_remaining -= tickets_needed
+        else:
+            print("Thank you, {}, for using the Master Ticket service. Your purchase has been cancelled.".format(user_name))
+print("Sorry, {}, there are not enough tickets left".format(user_name))
