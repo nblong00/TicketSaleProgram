565d3208da1c35fdc838491ff67f32e1e0ac7d0e
Blaise <blazzze0517@gmail.com>
2025-05-23 15:45:28 -0400

    Centralized everything into a 'main' function. Cleaned up formatting.

--- TicketSaleProgram.py
+++ TicketSaleProgram.py
@@ -1,21 +1,23 @@
-TICKET_PRICE = 10
-SERVICE_CHARGE = 2
 
-tickets_remaining = 100 
 
-def calculate_price(number_of_tickets):
-    return (number_of_tickets * TICKET_PRICE) + 2
+def main():
+    TICKET_PRICE = 10
+    SERVICE_CHARGE = 2
 
-while tickets_remaining != 0:
-    print("Number of tickets remaining: {}".format(tickets_remaining))
-    user_name = input("Enter your name: ")
-    try:
-        tickets_needed = int(input("Hello {}, how many tickets are you looking to purchase? ".format(user_name)))
-        if tickets_needed > tickets_remaining:
-            raise ValueError("Requested ticket amount is above what is available. Try again.")
-    except ValueError as e: 
-        print("Invalid entry. {}".format(e))
-    else:
+    tickets_remaining = 100 
+
+    def calculate_price(number_of_tickets):
+        return (number_of_tickets * TICKET_PRICE) + SERVICE_CHARGE
+
+    while tickets_remaining != 0:
+        print("Number of tickets remaining: {}".format(tickets_remaining))
+        user_name = input("Enter your name: ")
+        try:
+            tickets_needed = int(input("Hello {}, how many tickets are you looking to purchase? ".format(user_name)))
+            if tickets_needed > tickets_remaining:
+                raise ValueError("Requested ticket amount is above what is available. Try again.")
+        except ValueError as err: 
+            print("Invalid entry. {}".format(err))
         total_price = calculate_price(tickets_needed) 
         print("Your total is ${}".format(total_price))
         proceed = input("Would you like to proceed with purchase, {}?\n(Enter yes/no) ".format(user_name))
@@ -24,4 +26,9 @@ while tickets_remaining != 0:
             tickets_remaining -= tickets_needed
         else:
             print("Thank you, {}, for using the Master Ticket service. Your purchase has been cancelled.".format(user_name))
-print("Sorry, {}, there are not enough tickets left".format(user_name))
+            break
+    if tickets_remaining <= 0:
+        print("Sorry, {}, there are not enough tickets left".format(user_name))
+
+
+main()
